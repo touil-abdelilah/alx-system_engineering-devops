d1e14a3a054354aa11f3b5713645708e253a25ae
et.touil <et.touil505@gmail.com>
2024-02-28 01:50:33 +0100

    The regular expression must match a 10 digit phone number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,24 +1,2 @@
 #!/usr/bin/env ruby
-
-# Function to check if the input string matches the regular expression
-def match_pattern(str)
-  # Regular expression to match the pattern
-  regex = /^\d{10}$/
-
-  # Check if the input string matches the regular expression
-  if str.match?(regex)
-    puts str
-  end
-end
-
-# Main code
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit(1)
-end
-
-# Get the input string from the command-line argument
-input_str = ARGV[0]
-
-# Call the match_pattern method to check if the input string matches the regular expression
-match_pattern(input_str)
+puts ARGV[0].scan(/^\d{10}$/).join
