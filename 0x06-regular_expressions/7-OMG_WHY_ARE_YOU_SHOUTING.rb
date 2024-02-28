784ea96bdc3f5535451b137ab175af382cb5f639
et.touil <et.touil505@gmail.com>
2024-02-28 01:43:44 +0100

    7. OMG WHY ARE YOU SHOUTING?

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,25 +1,3 @@
 #!/usr/bin/env ruby
 
-# Function to extract capital letters from the input string
-def extract_capital_letters(str)
-  # Regular expression to match capital letters
-  regex = /[A-Z]/
-
-  # Extract capital letters from the input string
-  capital_letters = str.scan(regex).join
-
-  # Print the extracted capital letters
-  puts capital_letters
-end
-
-# Main code
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit(1)
-end
-
-# Get the input string from the command-line argument
-input_str = ARGV[0]
-
-# Call the extract_capital_letters method to extract capital letters from the input string
-extract_capital_letters(input_str)
+puts ARGV[0].scan(/[A-Z]/).join
