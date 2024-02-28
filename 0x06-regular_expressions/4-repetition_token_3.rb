b96c27afd3e3e7136d6172fd2f9c9e9c34c445e1
et.touil <et.touil505@gmail.com>
2024-02-28 02:13:53 +0100

    4. Repetition Token #3

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,25 +1,2 @@
 #!/usr/bin/env ruby
-
-# Function to check if the input string matches the regular expression
-def match_pattern(str)
-  # Regular expression to match the pattern
-  regex = /hbn|hbt{1,}n/
-
-  # Check if the input string matches the regular expression
-  if str.match?(regex)
-    puts str
-  end
-end
-
-# Main code
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit(1)
-end
-
-# Get the input string from the command-line argument
-input_str = ARGV[0]
-
-# Call the match_pattern method to check if the input string matches the regular expression
-match_pattern(input_str)
-
+puts ARGV[0].scan(/hb[^o]t{1,}n/).join("\n")
