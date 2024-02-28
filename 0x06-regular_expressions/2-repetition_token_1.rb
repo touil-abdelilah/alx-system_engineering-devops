bb7b7561b7f34ce1bc9bd8e2ef34b8a981886b8d
et.touil <et.touil505@gmail.com>
2024-02-28 01:29:45 +0100

    2. Repetition Token #1

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# Function to check if the input string matches the regular expression
+def match_pattern(str)
+  # Regular expression to match the pattern
+  regex = /hb{0,2}tn/
+
+  # Check if the input string matches the regular expression
+  if str.match?(regex)
+    puts str
+  end
+end
+
+# Main code
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit(1)
+end
+
+# Get the input string from the command-line argument
+input_str = ARGV[0]
+
+# Call the match_pattern method to check if the input string matches the regular expression
+match_pattern(input_str)
