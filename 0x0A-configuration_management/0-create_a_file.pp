ca84dcc596b74fd851f5226ca18707e802d2311e
et.touil <et.touil505@gmail.com>
2024-04-23 14:40:56 +0100

    Using Puppet, create a file in /tmp.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-file { '/tmp/school':
+$path = '/tmp/school'
+file { $path:
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
+
