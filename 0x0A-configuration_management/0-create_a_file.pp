a9d7b93fbadba56b11902069e4eedb9db1e81df8
et.touil <et.touil505@gmail.com>
2024-04-23 14:43:18 +0100

    Using Puppet, create a file in /tmp.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-$path = '/tmp/school'
-file { $path:
+# Puppet manifest to create a file in /tmp with specific permissions, owner, group, and content
+
+file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
 }
-
