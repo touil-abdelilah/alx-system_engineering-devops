30f54770e8fc26330d8d741a84416d57b2f6e02e
et.touil <et.touil505@gmail.com>
2024-04-23 15:21:55 +0100

    Puppet manifest to install Flask package from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,20 @@
-# Puppet manifest to install Flask package from pip3
+class python {
+  package { 'python3':
+    ensure => installed,
+  }
+}
 
-# Ensure Python 3 and pip3 are installed
-package { 'python3':
-  ensure => installed,
+# Install Flask 2.1.0 with pip3 on Ubuntu 20.04
+
+class { 'python': }
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Class['python'],
 }
 
 package { 'python3-pip':
   ensure => installed,
-  require => Package['python3'],
 }
 
-# Install Flask package with version 2.1.0 using pip3
-exec { 'install_flask':
-  command   => '/usr/bin/pip3 install --upgrade Flask==2.1.0',
-  path      => '/usr/bin',
-  unless    => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  provider  => 'shell',
-}
