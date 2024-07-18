d7a2a4f07d689438de390abaff10b9540ec10385
et.touil <et.touil505@gmail.com>
2024-07-18 21:43:08 +0100

    Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""
+this doc for module
+"""
+import requests
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    
-    import requests
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
-    # Send a GET request to the Reddit API to fetch the top 10 hot posts for the given subreddit
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},  # Include a custom User-Agent to avoid request issues
-        allow_redirects=False  # Prevent automatic redirection to handle invalid subreddits
-    )
-    
-    # Check if the response status code indicates an error or redirection
-    if sub_info.status_code >= 300:
-        print('None')  # Print 'None' if the subreddit is invalid or if there's an error
+
+def number_of_subscribers(subreddit):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
-        # Extract and print the titles of the top 10 hot posts
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        return 0
