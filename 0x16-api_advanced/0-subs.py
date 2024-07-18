d9ea2f63cad523481a23760e4efeb6d3991ca1fd
et.touil <et.touil505@gmail.com>
2024-07-18 21:35:14 +0100

    Write a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+this doc for module
+"""
+import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    # Send a GET request to the Reddit API for the subreddit information
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-
-    # If the response status code indicates an error (>= 300), return 0
-    if sub_info.status_code >= 300:
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
         return 0
-
-    # Extract and return the number of subscribers from the JSON response
-    return sub_info.json().get("data").get("subscribers")
