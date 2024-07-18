222d3d2cfd65080cb08117f5e41afaeab714e226
et.touil <et.touil505@gmail.com>
2024-07-18 21:48:43 +0100

    Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,6 @@ import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
-
 def number_of_subscribers(subreddit):
     """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
