14cbc452566f52313e3425e1074b8f348a160e23
et.touil <et.touil505@gmail.com>
2024-07-18 21:40:48 +0100

    Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """Module for task 1"""
 
+import requests
 
 def top_ten(subreddit):
     """Queries the Reddit API and returns the top 10 hot posts
     of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    
+    # Define the URL for fetching the top 10 hot posts
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
+    
+    # Check if the response status code indicates success
+    if response.status_code >= 300:
         print('None')
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        # Extract and print the titles of the top 10 hot posts
+        data = response.json().get("data", {}).get("children", [])
+        for child in data:
+            print(child.get("data", {}).get("title", "No title available"))
